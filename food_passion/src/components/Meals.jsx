112647b1188be53af3c5be1be8e35c1326412664
unknown <dive@et.esiea.fr>
2022-10-22 23:51:35 +0200

    Cards

--- food_passion/src/components/Meals.jsx
+++ food_passion/src/components/Meals.jsx
@@ -1,13 +1,38 @@
 // import React, { useContext } from 'react'; This is not necessary anymore
 // import { AppContext } from '../context'; This is the right one
 import { useGlobalContext } from '../context';
+import { BsHandThumbsUp } from 'react-icons/bs'
 const Meals = () => {
 
-  const context = useGlobalContext()
-  console.log(context)
+  const {loading, meals} = useGlobalContext();
 
+  if(loading) {
+    return <section className='section'>
+      <h4>Loading...</h4>
+    </section>
+  }
+
+  if ( meals.length < 1) {
+    return <section className='section'>
+      <h4>Oops no meals matched ! Please try another one. </h4>
+    </section>
+  }
   return (
-    <div>Meals</div>
+    <section className='section-center'>
+      {meals.map ((singleMeal) => {                                         // in map you can pass .map((argument, index)) and key this index
+        const { idMeal, strMeal: title, strMealThumb: image } = singleMeal; //Call the value we are gonna use and give alias to be simple
+
+        return <div key={idMeal} className='single-meal'>
+          <img src={image} className='img' />
+          <footer>
+            <h5>{title}</h5>
+            <button className='like-btn'><BsHandThumbsUp /></button>
+          </footer>
+        </div>
+      })
+
+      }
+      </section>
   )
 };
 
--- food_passion/src/context/Context.jsx
+++ food_passion/src/context/Context.jsx
@@ -1,27 +1,67 @@
 // This permits to put all the logic inside one folder
 
-import React, { useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import axios from 'axios';
 
 const AppContext = React.createContext()
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+const randomMeal = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 const AppProvider = ({children}) => { 
   
-  const fetchData = async() => {
-     try {
-       const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-       const data = await response.json();
-       console.log(data);
-     } catch (error) {
-       console(error);
-     }
-   }
+  
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchMeals = async (url) =>   {
+    setLoading(true)
+    try {
+      const {data} = await axios(url)
+      if (data.meals) { //here I check if data.meals is true
+        setMeals(data.meals)       
+      } else { 
+        setMeals([])
+      }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false)
+    }
+  
+    useEffect(() => {
+      fetchMeals(allMealsUrl)
+    },[]);
+    
+  return <AppContext.Provider value={{loading, meals}}>
+    {children}
+  </AppContext.Provider> 
+};
+
+// This is used to wrap our components with useContext but also AppContext !
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+}
+
+export { AppContext, AppProvider };
+
+
+// 2 ème méthode : BEST ONE out of this 2  ! 
+  // const fetchData = async() => {
+  //    try {
+  //      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+  //      const data = await response.json();
+  //      console.log(data);
+  //    } catch (error) {
+  //      console.log(error);
+  //    }
+  //  }
   
    // It cannot return a promise so no async -> to use async you need to call a function outside or inside useEffect
   // We can use .then or async/await
 
-   useEffect(() => { 
-    fetchData();
-   })
+  //  useEffect(() => { 
+  //   fetchData();
+  //  })
 
   // 1er method
   // useEffect (() => {
@@ -37,15 +77,4 @@ const AppProvider = ({children}) => {
   //   }
   //   fetchData();
   // },[])
-
-  return <AppContext.Provider value='hello'>
-    {children}
-  </AppContext.Provider> 
-};
-
-// This is used to wrap our components with useContext but also AppContext !
-export const useGlobalContext = () => {
-  return useContext(AppContext);
-}
-
-export { AppContext, AppProvider };
+  //
