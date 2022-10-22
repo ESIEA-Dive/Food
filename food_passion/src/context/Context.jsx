32e0cc5aa5ebdfaad8a38da2c65fe804d6947ffb
ESIEA-Dive <dive@et.esiea.fr>
2022-10-22 18:39:01 +0200

    Update Method

--- food_passion/src/context/Context.jsx
+++ food_passion/src/context/Context.jsx
@@ -4,7 +4,40 @@ import React, { useContext, useEffect } from 'react';
 
 const AppContext = React.createContext()
 
-const AppProvider = ({children}) => {
+const AppProvider = ({children}) => { 
+  
+  const fetchData = async() => {
+     try {
+       const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+       const data = await response.json();
+       console.log(data);
+     } catch (error) {
+       console(error);
+     }
+   }
+  
+   // It cannot return a promise so no async -> to use async you need to call a function outside or inside useEffect
+  // We can use .then or async/await
+
+   useEffect(() => { 
+    fetchData();
+   })
+
+  // 1er method
+  // useEffect (() => {
+    
+  //   const fetchData = async() => {
+  //     try {
+  //       const response = await fetch('https://randomuser.me/api/')
+  //       const data = await response.json();
+  //       console.log(data);
+  //     } catch (error) {
+  //       console(error);
+  //     }
+  //   }
+  //   fetchData();
+  // },[])
+
   return <AppContext.Provider value='hello'>
     {children}
   </AppContext.Provider> 
